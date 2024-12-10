c27762a5e40bd69708bc06dfd54e321da7f13f93
Subha Mondal <subha.snt2002@gmail.com>
2024-12-10 07:56:11 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Helllo World');
+})
+
+module.exports = app;
